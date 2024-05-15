f38ab17cd03053b451377723552c0194a6f4200e
Nirbhay Singh Narang <nsn8@cornell.edu>
2024-05-15 17:09:40 +0530

    Feature dump: Eleven labs, color grading, img shake, webp output.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 import uuid
 import requests
 from PIL import Image, ImageFont, ImageDraw
-from moviepy.editor import ImageSequenceClip, AudioFileClip, afx, concatenate_videoclips
+from moviepy.editor import ImageSequenceClip, AudioFileClip, afx, CompositeAudioClip
 import numpy as np
 import cv2
 import io
@@ -11,6 +11,13 @@ import os
 import json
 import random
 from gc import collect
+from elevenlabs.client import ElevenLabs
+
+from load_dotenv import load_dotenv
+from elevenlabs import play, stream, save
+
+
+
 
 app = Flask(__name__)
 
@@ -144,14 +151,24 @@ def wrap_text(text, font, max_width):
     
     return lines
 
+def shake_frame(frame, max_translation=100):
+    rows, cols, _ = frame.shape
+    translation_x = random.randint(-max_translation, max_translation) / 400
+    translation_y = random.randint(-max_translation, max_translation) / 400
+    
+    translation_matrix = np.float32([[1, 0, translation_x], [0, 1, translation_y]])
+    shaken_frame = cv2.warpAffine(frame, translation_matrix, (cols, rows))
+    
+    return shaken_frame
 
-def put_text_on_image(img, text, web_url="", phone_number="", font_size=32, text_color=(0, 0, 0, 200), bg_color=(255, 255, 255, 128)):
+
+def put_text_on_image(img, text, web_url="", phone_number="", font_size=32, text_color=(0, 0, 0, 200), bg_color=(255, 255, 255, 128), font="Tiro"):
     # Convert numpy array to PIL Image
     img_pil = Image.fromarray(img)
     draw = ImageDraw.Draw(img_pil, 'RGBA')
 
     # Use a Unicode compatible font
-    font_path = "./resources/NotoSansDevanagari-Bold.ttf"
+    font_path = "./resources/NotoSansDevanagari-Bold.ttf" if font!="Tiro" else "./resources/TiroDevanagariHindi-Regular.ttf"
     font = ImageFont.truetype(font_path, int(16))
 
     # top right
@@ -198,6 +215,22 @@ def put_text_on_image(img, text, web_url="", phone_number="", font_size=32, text
     return np.array(img_pil)
 
 
+
+def apply_color_grading(image, brightness=0, contrast=0, color_filter=None):
+ 
+    # Adjust brightness and contrast
+    image = cv2.convertScaleAbs(image, alpha=1 + contrast / 127.0, beta=brightness)
+
+    # Apply color filter
+    if color_filter:
+        b, g, r = cv2.split(image)
+        b = cv2.add(b, color_filter[0])
+        g = cv2.add(g, color_filter[1])
+        r = cv2.add(r, color_filter[2])
+        image = cv2.merge((b, g, r))
+
+    return image
+
 def ease_in_out(x):
     if x < 0.5:
         return 2 * x * x
@@ -228,41 +261,71 @@ def generate_zoom_pan_frames(np_img, num_frames):
         yield cropped_frame
 
 
-def create_slideshow(images, stories, audio, filename, web_url, phone_number, zoom_pan=True, slide_duration=10, fadein_duration=2, fadeout_duration=2, fps=24):
+def create_slideshow(images, stories, audio, filename, web_url, phone_number, zoom_pan=True, slide_duration=10, fadein_duration=2, fadeout_duration=2, fps=24, web_p=False, shake_effect=False, color_grading=True, font='Tiro', unique_id=None):
     clips = []
+    total_duration = 0 
     for img, story in zip(images, stories):
+        speak(story, unique_id)
+        #Read in the file output/audio/{unique_id}.mp3 here, calculate its duration
+        audio_speak = AudioFileClip(f"./output/audio/{unique_id}.mp3")
+        audio_duration = audio_speak.duration
+        total_duration += max(audio_duration, 10)
         np_img = np.array(img)
-        if (zoom_pan):
-            for frame in generate_zoom_pan_frames(np_img, int(slide_duration * fps)):
+        if zoom_pan:
+            for frame in generate_zoom_pan_frames(np_img, int(audio_duration * fps)):
+                if shake_effect:
+                    frame = shake_frame(frame)
                 processed_frame = put_text_on_image(frame, story, web_url, phone_number)
+                if web_p:
+                    processed_frame = np.array(Image.fromarray(processed_frame).resize((512, 512)))  # Resize to 512x512 if webp
+                processed_frame = apply_color_grading(processed_frame, 20,20,None) if color_grading else processed_frame
                 clips.append(processed_frame)
                 del frame  
         else:
-            clips.append(put_text_on_image(np_img, story))
-            fps = 1/slide_duration
+            processed_frame = put_text_on_image(np_img, story)
+            if web_p:
+                processed_frame = np.array(Image.fromarray(processed_frame).resize((512, 512)))  # Resize to 512x512 if webp bc whatsapp sticker 
+                processed_frame = apply_color_grading(processed_frame, 20,20,None) if color_grading else processed_frame
+            clips.append(processed_frame)
+            fps = 1 /  max(audio_duration, 10)
         del np_img  
         collect()
 
-    # Create video from frames
-    clip = ImageSequenceClip(clips, fps=fps)
-
-    # Load the background audio
-    if audio:
-        audio = afx.audio_loop(audio, slide_duration * len(images))
-        audio = audio.set_duration(clip.duration)
-        audio = audio.audio_fadein(fadein_duration).audio_fadeout(fadeout_duration)
-        clip = clip.set_audio(audio)
-
-    # Write the video file
-    clip.write_videofile(filename, fps=24, codec='libx264', audio_codec='aac')
+    if web_p:
+        webp_frames = [Image.fromarray(frame) for frame in clips]
+        webp_frames[0].save(filename, save_all=True, append_images=webp_frames[1:], duration=int(1000 / fps), loop=0, format='WEBP')
+    else:
+        clip = ImageSequenceClip(clips, fps=fps)
+        # Load the background audio
+        if audio:
+            background_audio = audio
     
+            background_audio = afx.audio_loop(background_audio, duration=total_duration)
+            background_audio = background_audio.set_duration(total_duration)
+            background_audio = background_audio.audio_fadein(fadein_duration).audio_fadeout(fadeout_duration)
+            
+            # Adjust the background audio volume to 25%
+            background_audio = background_audio.volumex(0.25)
+
+            # Combine the background audio and audio_speak
+            if audio_speak:
+                audio_speak = audio_speak.audio_fadein(fadein_duration).audio_fadeout(fadeout_duration)
+                composite_audio = CompositeAudioClip([background_audio, audio_speak])
+                
+            else:
+                composite_audio = background_audio
+            
+            clip = clip.set_audio(composite_audio)
+            
+        # Write the video file
+        clip.write_videofile(filename, fps=24, codec='libx264', audio_codec='aac')
+
     # Cleanup
-    # audio.close()  
     del clips  
     collect()  
 
 
-def generate_short_content(bearer_token, prompt, stories, system_prompt, img_prompt, style_prompt, web_url, phone_number, gen_images, zoom_pan, add_music, num_slides, num_chars, slide_duration, filename):
+def generate_short_content(bearer_token, prompt, stories, system_prompt, img_prompt, style_prompt, web_url, phone_number, gen_images, zoom_pan, add_music, num_slides, num_chars, slide_duration, filename, web_p, shake_effect, apply_color_grading, font, unique_id):
     
     if not len(stories):
         stories = generate_stories(bearer_token, prompt, system_prompt, num_slides, num_chars)
@@ -278,8 +341,29 @@ def generate_short_content(bearer_token, prompt, stories, system_prompt, img_pro
         files = get_random_files(directory_path)
         audio = AudioFileClip(os.path.join(directory_path, files[0]))
     
-    create_slideshow(images, stories, audio, filename, web_url, phone_number, zoom_pan, slide_duration)
+    create_slideshow(images, stories, audio, filename, web_url, phone_number, zoom_pan, slide_duration, web_p=web_p, shake_effect=shake_effect, color_grading=apply_color_grading, font=font, unique_id=unique_id)
     return stories
+
+
+def speak(text, fname):
+    client = ElevenLabs()
+    
+    audio = client.generate(
+        text=text,
+        voice=os.environ["ELEVEN_LABS_VOICE_ID"],
+        model="eleven_multilingual_v2"
+    )
+    
+    output_dir = "./output/audio/"
+    os.makedirs(output_dir, exist_ok=True)
+    
+    # Correct the save path in the save function
+    save_path = os.path.join(output_dir, f"{fname}.mp3")
+        
+    # Iterate over the generator to write the audio content
+    with open(save_path, "wb") as f:
+        for chunk in audio:
+            f.write(chunk)
 
 
 @app.route('/generate_video', methods=['POST'])
@@ -287,6 +371,7 @@ def generate_video():
     try:
         # Get bearer token from headers
         bearer_token = request.headers.get('Authorization')
+        
 
         if not bearer_token:
             return jsonify({"error": "Authorization token is missing"}), 401
@@ -306,27 +391,36 @@ def generate_video():
         style_prompt = data.get('style_prompt', "")
         web_url = data.get('web_url', "mahabot.in")
         phone_number = data.get('phone_number', "+919944044840")
+        
+        font = data.get("font", "Tiro")
+        apply_color_grading = data.get('apply_color_grading', True)
+        
+        web_p = data.get('web_p', False)
+        shake_effect = data.get("shake_effect", True)
+        
+        if web_p: assert(num_slides == 1) #only one slide if sticker.
 
         if not prompt and not len(stories):
             return jsonify({"error": "Prompt or stories must be provided"}), 400
 
         # Generate a unique filename for the video
         unique_id = str(uuid.uuid4())
-        filename = f"./output/videos/{unique_id}.mp4"
+        filename = f"./output/videos/{unique_id}.mp4" if not web_p else f"./output/videos/{unique_id}.webp"
 
         # Generate video based on the bearer token, prompt and/or stories
-        stories = generate_short_content(bearer_token, prompt, stories, system_prompt, img_prompt, style_prompt, web_url, phone_number, gen_images, zoom_pan, add_music, num_slides, num_chars, slide_duration, filename)
+        stories = generate_short_content(bearer_token, prompt, stories, system_prompt, img_prompt, style_prompt, web_url, phone_number, gen_images, zoom_pan, add_music, num_slides, num_chars, slide_duration, filename, web_p, shake_effect=shake_effect, apply_color_grading=apply_color_grading, font=font, unique_id=unique_id)
 
         # Check if the video file has been created and exists
 
         # Generate URL for the created video
         # video_url = url_for('get_video', video_id=unique_id, _external=True)
-        video_url = f"https://{request.host}/videos/{unique_id}.mp4"
+        video_url = f"https://{request.host}/videos/{unique_id}.mp4" if not web_p else f"./output/videos/{unique_id}.webp"
         return jsonify({"video_url": video_url, "stories": stories})
     
     except Exception as e:
         # Log the exception, could be more detailed depending on the logging setup
         app.logger.error(f"Failed to generate video: {str(e)}")
+        app.log_exception(e)
         # Return a generic error message and a 500 Internal Server Error status code
         return jsonify({"error": "An error occurred while processing your request"}), 500
 
@@ -345,5 +439,7 @@ def home():
     return "Welcome to the Video Generator API!"
 
 if __name__ == '__main__':
+    load_dotenv() #Load env
+    
     port = int(os.environ.get('PORT', 6000))  
     app.run(debug=True, host='0.0.0.0', port=port)
